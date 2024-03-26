63ac70e9158da05e1235537d146f738559002af5
Viktor Rura <ruravictor@gmail.com>
2024-03-26 16:01:34 +0000

    Updated static root in settings.

--- nomads_alchemy/settings.py
+++ nomads_alchemy/settings.py
@@ -142,7 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 USE_AWS = os.getenv("USE_AWS", False)
 
